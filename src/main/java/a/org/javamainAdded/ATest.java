322ae4c5561f2bbb694b9394e0ac2299dfbddf6d
universalcodes <yogeshkamra25@gmail.com>
2026-07-22 14:37:12 +0530

    Day-1 Push Code To See Results In Jenkins 5

--- src/main/java/a/org/javamainAdded/ATest.java
+++ src/main/java/a/org/javamainAdded/ATest.java
@@ -3,5 +3,6 @@ package a.org.javamainAdded;
 public class ATest {
     public static void main(String[] args) {
         System.out.println("Java Src main Called");
+        System.out.println("Java Src main Called2");
     }
 }
