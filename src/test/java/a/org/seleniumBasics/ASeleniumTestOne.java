68373aa7b6afa4b7e99b8c335673a1bd3c1a262c
universalcodes <yogeshkamra25@gmail.com>
2026-07-22 13:44:12 +0530

    Day-1 Selenium Started With Maven & Along with Jenkins

---
+++ src/test/java/a/org/seleniumBasics/ASeleniumTestOne.java
@@ -0,0 +1,42 @@
+package a.org.seleniumBasics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ASeleniumTestOne {
+
+    public static void main(String[] args) {
+        /*
+        Open All Browsers and closed the same
+         */
+
+        System.out.println("1. Launch Google Chrome Browser Through Selenium");
+        WebDriver googleChromeWebDriver = new ChromeDriver();
+        googleChromeWebDriver.get("https://www.amazon.in/ref=nav_logo");
+        System.out.println("Launched Google Chrome Browser Through Selenium Successfully");
+        googleChromeWebDriver.quit();
+        System.out.println("Closed Google Chrome Browser Through Selenium Successfully");
+
+
+        System.out.println("2. Launch Firefox Browser Through Selenium");
+        WebDriver firefoxWebDriver = new FirefoxDriver();
+        firefoxWebDriver.get("https://www.amazon.in/ref=nav_logo");
+        System.out.println("Launched Firefox Browser Through Selenium Successfully");
+        firefoxWebDriver.quit();
+        System.out.println("Closed Firefox Browser Through Selenium Successfully");
+
+
+
+
+        System.out.println("3. Launch Edge Browser Through Selenium");
+        WebDriver edgeWebDriver = new EdgeDriver();
+        edgeWebDriver.get("https://www.amazon.in/ref=nav_logo");
+        System.out.println("Launched Edge Browser Through Selenium Successfully");
+        edgeWebDriver.quit();
+        System.out.println("Closed Edge Browser Through Selenium Successfully");
+
+
+    }
+}
